78e156f3d68a2d55d8ae41ca368eec7ad303261c
SARAN INNAREE <saran.inna@bulive.net>
2016-03-30 10:40:52 +0700

    JAM01

---
+++ UFO3/GameJam2/Assets/SpawnCube.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnCube : MonoBehaviour {
+	public GameObject newCube; 
+	public GameObject newposition; 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Instantiate (newCube, newposition.transform.position.x, newposition.transform.position.y, newposition.transform.position.z);
+	
+	}
+}
